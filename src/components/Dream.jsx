b789c202c824ebf1c76298d293b8938eef7b20a6
Alex K <isaev.msksim84@gmail.com>
2024-02-21 15:43:34 +0100

    commit2

--- src/components/Dream.jsx
+++ src/components/Dream.jsx
@@ -1,20 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import { imagesArr } from './sombodey';
+import img1 from '../assets/images/friend1.jpg';
+import img2 from '../assets/images/friend2.jpg';
+import img3 from '../assets/images/friend3.jpg';
+import img4 from '../assets/images/friend4.jpg';
+import img5 from '../assets/images/friend5.jpg';
+import img6 from '../assets/images/friend6.jpg';
+import img7 from '../assets/images/friend7.jpg';
+import img8 from '../assets/images/friend8.jpg';
+import img9 from '../assets/images/friend9.jpg';
 
 export const Dream = () => {
-
-    const images = imagesArr;
-    console.log(images);
-
-    const [data, setData] = useState([images]);
     
-   
-
-
     return (
         <section className="right img">
             <h2>Dream team</h2>
-            {data.map((item, key) => <img src={item} key={key} />)}
+            <img src={img1} />
+            <img src={img2} />
+            <img src={img3} />
+            <img src={img4} />
+            <img src={img5} />
+            <img src={img6} />
+            <img src={img7} />
+            <img src={img8} />
+            <img src={img9} />
         </section>
     )
 }
